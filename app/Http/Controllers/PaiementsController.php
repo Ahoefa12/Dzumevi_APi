5e9815ec0cb1788c1c02b687af8407e2ddbeefba
ALASSANE Paul <alassanepaul1.com>
2025-11-12 10:46:43 +0000

    implémentation retouche de paiementsController

--- app/Http/Controllers/PaiementsController.php
+++ app/Http/Controllers/PaiementsController.php
@@ -42,45 +42,6 @@ class PaiementsController extends Controller
             ]
         ]);
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-         
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        
-
         /* Créer une transaction */
         $transaction = Transaction::create([
             'description' => $validated['description'],
